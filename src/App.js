aeeedaf23e8d00590f9d8368a6a9f71e3806a064
Frederick Johnson <fritzj96@gmail.com>
2020-01-02 10:42:27 -0800

    added: an (almost) full crud component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (<div></div>);
+  const [branches, setBranches] = useState([]);
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+
+  useEffect(() => {
+    fetch("https://staging-cohort-bank.herokuapp.com/branches").then(res => {
+      res.json().then(data => {
+        setBranches(data);
+      });
+    });
+  }, []);
+
+  const deleteBranch = uid => {
+    fetch(`https://staging-cohort-bank.herokuapp.com/branches/${uid}`, {
+      method: "DELETE"
+    }).then(() =>
+      setBranches(branches.filter(branch => !(branch.uid === uid)))
+    );
+  };
+
+  const createBranch = (event) => {
+    event.preventDefault()
+    fetch(`https://staging-cohort-bank.herokuapp.com/branches/`, {
+      method: "POST",
+      body: JSON.stringify({address, name}),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(res => {
+      res.json(data => {
+        setBranches([...branches, data]);
+      });
+    });
+  };
+  const renderBranches = branchList => {
+    return branchList.map(branch => (
+      <li key={branch.uid}>
+        {branch.name} - {branch.address} -{" "}
+        <button
+          style={{ backgroundColor: "red" }}
+          onClick={() => {
+            console.log("works");
+            deleteBranch(branch.uid);
+          }}
+        >
+          Delete
+        </button>
+      </li>
+    ));
+  };
+
+  return (
+    <div>
+      <form onSubmit={event => createBranch(event)}>
+        <input placeholder="Branch Name" value={name} onChange={event => setName(event.target.value)} />
+        <input placeholder="Branch Address" value={address} onChange={event => setAddress(event.target.value)} />
+        <button type="submit">Add New Branch</button>
+      </form>
+      <ul>{renderBranches(branches)}</ul>
+    </div>
+  );
 }
 
 export default App;
