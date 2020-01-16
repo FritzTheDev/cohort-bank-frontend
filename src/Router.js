6da1f04f36a7ac7b4ec79f2fbc1e302e32f234f7
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 12:11:16 -0800

    added: homepage to router

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { BranchListPage } from "./pages/branch-list.page";
+import { HomePage } from "./pages/home.page";
 import { Navigation } from "./components/Navigation";
 
 export const Router = () => {
   return (
       <BrowserRouter>
         <Navigation />
+        <Route path="/" component={HomePage} />
         <Route path="/branches" component={BranchListPage} />
       </BrowserRouter>
   );
