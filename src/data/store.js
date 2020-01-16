68e943224f9ef311aed62a92c4ec46e17f99e90a
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 11:11:09 -0800

    added: redux store

--- src/data/store.js
+++ src/data/store.js
@@ -1,1 +1,6 @@
-export const 
+import { createStore, applyMiddleware } from "redux";
+import Thunk from "redux-thunk";
+
+import { rootReducer } from "./reducers";
+
+export const store = createStore(rootReducer, applyMiddleware(Thunk));
