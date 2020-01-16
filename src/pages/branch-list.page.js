9c5e71e6795d5b99ffde69f57b4dee76bbb3c011
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 13:29:55 -0800

    added: renderBranches

--- src/pages/branch-list.page.js
+++ src/pages/branch-list.page.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Button, Col, Row } from "react-bootstrap";
 import { getBranches } from "../data/actions/branch.actions";
 
 export const BaseBranchListPage = ({
@@ -13,9 +13,33 @@ export const BaseBranchListPage = ({
     getAllBranches();
   }, [getAllBranches]);
 
+  const renderBranches = () => {
+    return branches.map(branch => (
+      <Col key={branch.id} xs="12" md={{ span: 4 }}>
+        <Card>
+          <Card.Header>
+            <Card.Title className="text-center">{branch.name}</Card.Title>
+            <Card.Subtitle className="text-center">
+              {branch.address}
+            </Card.Subtitle>
+          </Card.Header>
+          <Card.Body className="text-center">
+            <Button variant="success" className="mx-2">
+              Edit
+            </Button>
+            <Button variant="danger" className="mx-2">
+              Delete
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
+  };
+
   return (
     <Container>
       <h1 className="text-center m-4">Branches</h1>
+      <Row>{!error && !loading && renderBranches()}</Row>
     </Container>
   );
 };
