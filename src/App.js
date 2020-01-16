484e01eab09a51dab267ee46b2650a4b2fc058ef
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 11:43:02 -0800

    added: Route with BranchListPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { store } from "./data/store";
+import { BranchListPage } from "./pages/branch-list.page";
 
 export const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Route path="/" component={} />
+        <Route path="/" component={BranchListPage} />
       </Router>
     </Provider>
   );
