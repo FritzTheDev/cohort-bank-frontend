5ee7d8451661d9cbbb8b13152687b194dab1b200
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 11:33:08 -0800

    added: base App component

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import { store } from "./data/store";
+
+export const App = () => {
+  return <Provider store={store}></Provider>
+}
