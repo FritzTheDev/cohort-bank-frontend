9e17045699d6ee91fcc05946f4c2d4a67adfa4f2
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 15:44:03 -0800

    added: jsx for a login page

--- src/pages/login.page.js
+++ src/pages/login.page.js
@@ -1,3 +1,31 @@
 import React from "react";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 
-export const LoginPage = () => <div /> 
+export const LoginPage = () => {
+  return (
+    <Container className="my-5">
+      <Row>
+        <Col md={{ span: 4, offset: 4 }}>
+          <Card>
+            <Card.Header>
+              <Card.Title className="text-center my-0">Log In</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Form className="text-center">
+                <Form.Group>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+                <Button type="submit">Log in</Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
