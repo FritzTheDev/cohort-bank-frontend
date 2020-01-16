ff5f6aaed45694a27db06ceee997761276a62dcb
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 15:51:52 -0800

    fixed: preventDefault

--- src/pages/login.page.js
+++ src/pages/login.page.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 
-export const LoginPage = () => {
+import { login } from "../data/actions/auth.actions";
+import { connect } from "react-redux";
+
+const BaseLoginPage = ({ logIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+    logIn(username, password);
+    setUsername("");
+    setPassword("");
+  }
+
   return (
     <Container className="my-5">
       <Row>
@@ -11,13 +24,13 @@ export const LoginPage = () => {
               <Card.Title className="text-center my-0">Log In</Card.Title>
             </Card.Header>
             <Card.Body>
-              <Form className="text-center">
+              <Form onSubmit={event => onSubmit(event)} className="text-center">
                 <Form.Group>
                   <Form.Label>Username</Form.Label>
                   <Form.Control />
                 </Form.Group>
                 <Form.Group>
-                  <Form.Label>Email</Form.Label>
+                  <Form.Label>Password</Form.Label>
                   <Form.Control />
                 </Form.Group>
                 <Button type="submit">Log in</Button>
@@ -29,3 +42,11 @@ export const LoginPage = () => {
     </Container>
   );
 };
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logIn: (username, password) => dispatch(login(username, password))
+  }
+}
+
+export const LoginPage = connect(null, mapDispatchToProps)(BaseLoginPage)
