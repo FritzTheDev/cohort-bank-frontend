363a4c9ecec925ab346eeca6023954846d3258e6
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 11:29:36 -0800

    added: branch reducer

--- src/data/reducers/branch.reducer.js
+++ src/data/reducers/branch.reducer.js
@@ -1,5 +1,22 @@
-export const branchReducer = (state=[], action) => {
+import { GET_BRANCHES_ATTEMPT, GET_BRANCHES_SUCCESS, GET_BRANCHES_FAILURE } from "../actions/branch.actions";
+
+const INITIAL_STATE = {
+  loading: false,
+  branches: [],
+  error: null
+}
+
+export const branchReducer = (state=INITIAL_STATE, action) => {
   switch (action.type) {
+    case GET_BRANCHES_ATTEMPT:
+      return Object.assign({}, state, { loading: true });
+
+    case GET_BRANCHES_SUCCESS:
+      return Object.assign({}, state, { loading: false, branches: action.payload });
+
+    case GET_BRANCHES_FAILURE:
+      return Object.assign({}, state, { loading: false, error: action.payload });
+      
     default:
       return state
   }
