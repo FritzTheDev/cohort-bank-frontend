7fd393dd0e530e8ac43b582cc45fee34a209516e
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 11:24:22 -0800

    added: getBranches action thunk

---
+++ src/data/actions/branch.actions.js
@@ -0,0 +1,37 @@
+import Axios from "axios";
+
+export const GET_BRANCHES_ATTEMPT = "GET_BRANCHES_ATTEMPT";
+export const GET_BRANCHES_SUCCESS = "GET_BRANCHES_SUCCESS";
+export const GET_BRANCHES_FAILURE = "GET_BRANCHES_FAILURE";
+
+const requestBranches = () => {
+  return {
+    type: GET_BRANCHES_ATTEMPT
+  }
+}
+
+const recieveBranches = branches => {
+  return {
+    type: GET_BRANCHES_SUCCESS,
+    branches
+  }
+}
+
+const branchesError = message => {
+  return {
+    type: GET_BRANCHES_FAILURE,
+    message
+  }
+}
+
+export const getBranches = () => dispatch => {
+  dispatch(requestBranches());
+  Axios.get('https://staging-cohort-bank.herokuapp.com/branches/')
+    .then(response => {
+      dispatch(recieveBranches(response.data));
+    })
+    .catch(error => {
+      console.log(error);
+      dispatch(branchesError(error.message));
+    });
+}
