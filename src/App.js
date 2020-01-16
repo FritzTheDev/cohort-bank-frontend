00994e7c75983816e28b0fdc4e2094b97c41821a
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 12:09:17 -0800

    added: App for redux & router for browserrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { store } from "./data/store";
+import { Router } from "./Router";
 
-import { BranchListPage } from "./pages/branch-list.page";
-
-import { Navigation } from "./components/Navigation";
-
-export const App = () => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Navigation />
-        <Route path="/branches" component={BranchListPage} />
-      </Router>
-    </Provider>
-  );
-};
+export const App = () => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+)
