9008bfc7dce53a1058ad6c7c3152201b27da8d5c
Frederick Johnson <fritzj96@gmail.com>
2020-01-17 08:16:10 -0800

    added: add_branch functions to the reducer

--- src/data/reducers/branch.reducer.js
+++ src/data/reducers/branch.reducer.js
@@ -1,23 +1,49 @@
-import { GET_BRANCHES_ATTEMPT, GET_BRANCHES_SUCCESS, GET_BRANCHES_FAILURE } from "../actions/branch.actions";
+import {
+  GET_BRANCHES_ATTEMPT,
+  GET_BRANCHES_SUCCESS,
+  GET_BRANCHES_FAILURE,
+  ADD_BRANCH_ATTEMPT,
+  ADD_BRANCH_SUCCESS,
+  ADD_BRANCH_FAILURE
+} from "../actions/branch.actions";
 
 const INITIAL_STATE = {
   loading: false,
+  newBranchLoading: false,
   branches: [],
   error: null
-}
+};
 
-export const branchReducer = (state=INITIAL_STATE, action) => {
+export const branchReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case GET_BRANCHES_ATTEMPT:
       return Object.assign({}, state, { loading: true });
 
     case GET_BRANCHES_SUCCESS:
-      return Object.assign({}, state, { loading: false, branches: action.payload });
+      return Object.assign({}, state, {
+        loading: false,
+        branches: action.payload
+      });
 
     case GET_BRANCHES_FAILURE:
-      return Object.assign({}, state, { loading: false, error: action.payload });
-      
+      return Object.assign({}, state, {
+        loading: false,
+        error: action.payload
+      });
+
+    case ADD_BRANCH_ATTEMPT:
+      return Object.assign({}, state, { newBranchLoading: true, error: null });
+
+    case ADD_BRANCH_SUCCESS:
+      return Object.assign({}, state, {
+        newBranchLoading: false,
+        branches: [...state.branches, action.payload]
+      });
+
+    case ADD_BRANCH_FAILURE:
+      return Object.assign({}, state, { error: action.payload });
+
     default:
-      return state
+      return state;
   }
-}
+};
