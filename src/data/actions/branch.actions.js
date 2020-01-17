44451078c7bb83535bac92df4ef48e0e1f35bce4
Frederick Johnson <fritzj96@gmail.com>
2020-01-17 08:12:18 -0800

    added: add_branch actions

--- src/data/actions/branch.actions.js
+++ src/data/actions/branch.actions.js
@@ -4,13 +4,17 @@ export const GET_BRANCHES_ATTEMPT = "GET_BRANCHES_ATTEMPT";
 export const GET_BRANCHES_SUCCESS = "GET_BRANCHES_SUCCESS";
 export const GET_BRANCHES_FAILURE = "GET_BRANCHES_FAILURE";
 
+export const ADD_BRANCH_ATTEMPT = "ADD_BRANCH_ATTEMPT";
+export const ADD_BRANCH_SUCCESS = "ADD_BRANCH_SUCCESS";
+export const ADD_BRANCH_FAILURE = "ADD_BRANCH_FAILURE";
+
 const requestBranches = () => {
   return {
     type: GET_BRANCHES_ATTEMPT
   }
 }
 
-const recieveBranches = branches => {
+const receiveBranches = branches => {
   return {
     type: GET_BRANCHES_SUCCESS,
     payload: branches
@@ -30,7 +34,7 @@ export const getBranches = () => (dispatch, getState) => {
     authorization: `Bearer ${getState().auth.token}`
   }})
     .then(response => {
-      dispatch(recieveBranches(response.data));
+      dispatch(receiveBranches(response.data));
     })
     .catch(error => {
       console.log(error);
