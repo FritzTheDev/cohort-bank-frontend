6f1c371e1b91c9a183fc61813ff18dd801761883
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 16:11:52 -0800

    removed: home page

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
 import { Navigation } from "./components/Navigation";
 
 import { BranchListPage } from "./pages/branch-list.page";
-import { HomePage } from "./pages/home.page";
 import { LoginPage } from "./pages/login.page";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Navigation />
-      <Route path="/" component={HomePage} />
+      <Route path="/" render={() => <Redirect to="/login" />} />
       <Route path="/login" component={LoginPage} />
       <Route path="/branches" component={BranchListPage} />
     </BrowserRouter>
